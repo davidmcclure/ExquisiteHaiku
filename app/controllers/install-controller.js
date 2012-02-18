1944d2e28ec2b9bc5cb7991d7397cecf1c2dacc4
David McClure <david.mcclure@virginia.edu>
2012-02-18 16:08:54 -0500

    install controller back in place

--- app/controllers/install-controller.js
+++ app/controllers/install-controller.js
@@ -46,70 +46,43 @@ module.exports = function(app) {
   app.post('/admin/install',
     auth.noUsers,
     function(req, res) {
-      res.send('POST /admin/install');
+
+      installForm.form().handle(req, {
+
+        // If validations pass.
+        success: function(form) {
+
+          // Create the user.
+          var user = new User({
+              username:   form.data.username,
+              email:      form.data.email,
+              password:   form.data.password,
+              superUser:  true,
+              active:     true
+          });
+
+          // Save and redirect.
+          user.save(function() {
+              req.session.user_id = user.id;
+              res.redirect('/admin');
+          });
+
+        },
+
+        // If validations fail.
+        other: function(form) {
+
+          // Re-render form.
+          res.render('auth/install', {
+            title:  'Installation',
+            form:   form,
+            layout: '_layouts/auth'
+          });
+
+        }
+
+      });
+
   });
-
-
-
-
-    /*
-     * Show install form.
-     *
-     * - middleware auth.noUsers: Block if there are any users in the
-     *   database.
-     */
-    // app.get('/admin/install', auth.noUsers, function(req, res) {
-    //     res.render('auth/install', {
-    //         title:      'Installation',
-    //         form:       forms.authForms.install(),
-    //         layout:     '_layouts/auth'
-    //     });
-    // });
-
-
-    /*
-     * Process install form.
-     *
-     * - middleware auth.noUsers: Block if there are any users in the
-     *   database.
-     */
-    // app.post('/admin/install', auth.anonUser, function(req, res) {
-
-    //     // Pass control to form.
-    //     forms.authForms.install().handle(req, {
-
-    //         // If field validations pass.
-    //         success: function(form) {
-
-    //             // Create the user.
-    //             var user = new User({
-    //                 username:   form.data.username,
-    //                 email:      form.data.email,
-    //                 password:   form.data.password,
-    //                 superUser:  true,
-    //                 active:     true
-    //             });
-
-    //             // Save and redirect.
-    //             user.save(function() {
-    //                 req.session.user_id = user.id;
-    //                 res.redirect('/admin');
-    //             });
-
-    //         },
-
-    //         // If field validations fail.
-    //         other: function(form) {
-    //             res.render('auth/install', {
-    //                 title:      'Install',
-    //                 form:       form,
-    //                 layout:     '_layouts/auth'
-    //             });
-    //         }
-
-    //     });
-
-    // });
-
 
 };

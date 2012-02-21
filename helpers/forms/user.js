89b83e959a8ae38b3d712ca7af4d9eafef596fcc
David McClure <david.mcclure@virginia.edu>
2012-02-20 20:00:07 -0500

    user edit forms

--- helpers/forms/user.js
+++ helpers/forms/user.js
@@ -77,3 +77,82 @@ exports.newUser = function() {
   });
 
 };
+
+
+exports.editInfo = function(user) {
+
+  return forms.create({
+
+    // Username.
+    username: fields.string({
+      name: 'username',
+      label: 'Username: *',
+      required: 'Enter a username.',
+      validators: [
+        validators.rangeLength(4, 20, '4-20 characters.'),
+        customValidators.uniqueNonSelfField(
+          User,
+          'username',
+          user,
+          'Username taken.'
+        )
+      ]
+    }),
+
+    // Email.
+    email: fields.email({
+      name: 'email',
+      label: 'Email: *',
+      required: 'Enter an email address.',
+      validators: [
+        customValidators.uniqueNonSelfField(
+          User,
+          'email',
+          user,
+          'Username taken.'
+        )
+      ]
+    }),
+
+    // Super/regular.
+    superUser: fields.boolean({
+        label: 'Super User:'
+    }),
+
+    // Active/inactive.
+    active: fields.boolean({
+        label: 'Active:'
+    })
+
+  });
+
+};
+
+
+exports.editPassword = function() {
+
+  return forms.create({
+
+    // Password.
+    password: fields.password({
+      name: 'password',
+      label: 'Password: *',
+      required: 'Enter a password.',
+      validators: [
+        validators.minLength(6, 'At least 6 characters.')
+      ]
+    }),
+
+    // Password confirmation.
+    confirm: fields.password({
+      name: 'confirm',
+      label: 'Retype Password: *',
+      required: 'Confirm the password.',
+      validators: [
+        validators.matchField('password', 'Does not match.')
+      ]
+    })
+
+  });
+
+};

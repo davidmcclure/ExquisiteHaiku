afa238ecd3fb593ab1279d4a61f5f5023e617b3a
David McClure <david.mcclure@virginia.edu>
2012-02-20 20:55:17 -0500

    user form tests done

--- test/unit/forms/user.test.js
+++ test/unit/forms/user.test.js
@@ -324,12 +324,9 @@ describe('User Forms', function() {
 
       it('should validate when it matches current username', function(done) {
 
-        console.log('before');
-
         form.bind({
           username: 'david'
         }).validate(function(err, form) {
-          console.log(form.fields.username);
           assert(!form.fields.username.error);
           done();
         });
@@ -340,15 +337,74 @@ describe('User Forms', function() {
 
     describe('email', function() {
 
-      it('should exist');
+      it('should exist', function(done) {
 
-      it('should be unique');
+        form.bind({
+          email: ''
+        }).validate(function(err, form) {
+          form.fields.email.error.should.be.ok;
+          done();
+        });
 
-      it('should be valid');
+      });
 
-      it('should validate when it does not match current email');
+      it('should be unique', function(done) {
 
-      it('should validate when it matches current email');
+        // Create a user.
+        var user = new User({
+          username:   'kara',
+          email:      'kara@spyder.com',
+          password:   'password',
+          superUser:  true,
+          active:     true
+        });
+
+        // Save.
+        user.save(function(err) {
+
+          form.bind({
+            email: 'kara@spyder.com'
+          }).validate(function(err, form) {
+            form.fields.email.error.should.be.ok;
+            done();
+          });
+
+        });
+
+      });
+
+      it('should be valid', function(done) {
+
+        form.bind({
+          email: 'invalid'
+        }).validate(function(err, form) {
+          form.fields.email.error.should.be.ok;
+          done();
+        });
+
+      });
+
+      it('should validate when it does not match current email', function(done) {
+
+        form.bind({
+          email: 'rosie@spyder.com'
+        }).validate(function(err, form) {
+          assert(!form.fields.email.error);
+          done();
+        });
+
+      });
+
+      it('should validate when it matches current email', function(done) {
+
+        form.bind({
+          email: 'david@spyder.com'
+        }).validate(function(err, form) {
+          assert(!form.fields.email.error);
+          done();
+        });
+
+      });
 
     });
 
@@ -356,27 +412,98 @@ describe('User Forms', function() {
 
   describe('editPassword', function() {
 
-    beforeEach(function() {
-      form = userForms.newUser();
+    beforeEach(function(done) {
+
+      // Create a user.
+      var user = new User({
+        username:   'david',
+        email:      'david@spyder.com',
+        password:   'password',
+        superUser:  true,
+        active:     true
+      });
+
+      // Save and create form.
+      user.save(function(err) {
+        form = userForms.editPassword(user);
+        done();
+      });
+
     });
 
     describe('password', function() {
 
-      it('should exist');
+      it('should exist', function(done) {
 
-      it('should be <= 6 characters');
+        form.bind({
+          password: ''
+        }).validate(function(err, form) {
+          form.fields.password.error.should.be.ok;
+          done();
+        });
 
-      it('should validate when valid');
+      });
+
+      it('should be <= 6 characters', function(done) {
+
+        form.bind({
+          password: 'pass'
+        }).validate(function(err, form) {
+          form.fields.password.error.should.be.ok;
+          done();
+        });
+
+      });
+
+      it('should validate when valid', function(done) {
+
+        form.bind({
+          password: 'password'
+        }).validate(function(err, form) {
+          assert(!form.fields.password.error);
+          done();
+        });
+
+      });
 
     });
 
     describe('confirm', function() {
 
-      it('should exist');
+      it('should exist', function(done) {
 
-      it('should match the password');
+        form.bind({
+          confirm: ''
+        }).validate(function(err, form) {
+          form.fields.confirm.error.should.be.ok;
+          done();
+        });
 
-      it('should validate when valid');
+      });
+
+      it('should match the password', function(done) {
+
+        form.bind({
+          password: 'password',
+          confirm: 'mismatch'
+        }).validate(function(err, form) {
+          form.fields.confirm.error.should.be.ok;
+          done();
+        });
+
+      });
+
+      it('should validate when valid', function(done) {
+
+        form.bind({
+          password: 'password',
+          confirm: 'password'
+        }).validate(function(err, form) {
+          assert(!form.fields.confirm.error);
+          done();
+        });
+
+      });
 
     });
 

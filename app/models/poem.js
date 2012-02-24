e4ad6768be1c975f9a8994cd4af43e7f84f6f901
David McClure <david.mcclure@virginia.edu>
2012-02-24 17:14:56 -0500

    adds poem model

---
+++ app/models/poem.js
@@ -0,0 +1,38 @@
+/*
+ * Poem model.
+ */
+
+// Schema definition.
+var Poem = new Schema({
+  slug :            { type: String, required: true },
+  user :            { type: Schema.ObjectId, ref: 'User', required: true },
+  created :         { type: Date, required: true, default: Date.now() },
+  roundLength :     { type: Number, required: true },
+  sliceInterval :   { type: Number, required: true },
+  minSubmissions :  { type: Number, required: true },
+  submissionVal :   { type: Number, required: true },
+  decayLifetime :   { type: Number, required: true },
+  seedCapital :     { type: Number, required: true }
+});
+
+
+/*
+ * -----------------
+ * Document methods.
+ * -----------------
+ */
+
+
+/*
+ * Get id.
+ *
+ * @return {String}: The id.
+ */
+Poem.virtual('id').get(function() {
+  return this._id.toHexString();
+});
+
+
+// Register model.
+mongoose.model('Poem', Poem);
+var Poem = mongoose.model('Poem');

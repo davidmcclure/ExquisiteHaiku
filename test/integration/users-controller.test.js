3451550c0cd4f8832f5a39e02436d841a1fc079a
David McClure <david.mcclure@virginia.edu>
2012-02-24 14:08:01 -0500

    blocking out user integration tests

--- test/integration/users-controller.test.js
+++ test/integration/users-controller.test.js
@@ -48,7 +48,7 @@ describe('User Controller', function() {
       password:   'password',
       email:      'kara@test.com',
       superUser:  true,
-      active:     true
+      active:     false
     });
 
     // Create user3.
@@ -57,7 +57,7 @@ describe('User Controller', function() {
       password:   'password',
       email:      'rosie@test.com',
       superUser:  true,
-      active:     true
+      active:     false
     });
 
     // Save worker.
@@ -516,6 +516,107 @@ describe('User Controller', function() {
 
   describe('GET /admin/users/edit/:username', function() {
 
+    it('should render the edit information form', function(done) {
+
+      // Go to non-self user edit page.
+      browser.visit(r+'admin/users/edit/kara', function() {
+
+        // Check for form.
+        browser.query('form.edit-info').should.be.ok;
+
+        // Username input and value.
+        browser.query(
+          'form.edit-info input[name="username"][value="kara"]'
+        ).should.be.ok;
+
+        // Email input and value.
+        browser.query(
+          'form.edit-info input[name="email"][value="kara@test.com"]'
+        ).should.be.ok;
+
+        // Super user input and value.
+        browser.query(
+          'form.edit-info input[name="superUser"][checked]'
+        ).should.be.ok;
+
+        // Active input and value.
+        browser.query(
+          'form.edit-info input[name="active"]'
+        ).should.be.ok;
+
+        // Confirm that the active checkbox is unchecked.
+        assert(!browser.query(
+          'form.edit-info input[name="active"][checked]'
+        ));
+
+        done();
+
+      });
+
+    });
+
+    it('should not render the Super User and Active checkboxes for self edit', function(done) {
+
+      // Go to self user edit page.
+      browser.visit(r+'admin/users/edit/david', function() {
+
+        // Check for form.
+        browser.query('form.edit-info').should.be.ok;
+
+        // Username input and value.
+        browser.query(
+          'form.edit-info input[name="username"][value="david"]'
+        ).should.be.ok;
+
+        // Email input and value.
+        browser.query(
+          'form.edit-info input[name="email"][value="david@test.com"]'
+        ).should.be.ok;
+
+        // Super user input should not be rendered.
+        assert(!browser.query(
+          'form.edit-info input[name="superUser"]'
+        ));
+
+        // Active input should not be rendered.
+        assert(!browser.query(
+          'form.edit-info input[name="active"]'
+        ));
+
+        done();
+
+      });
+
+    });
+
+    it('should render the edit password form', function(done) {
+
+      // Go to non-self user edit page.
+      browser.visit(r+'admin/users/edit/kara', function() {
+
+        // Check for form.
+        browser.query('form.edit-password').should.be.ok;
+
+        // Password input and value.
+        browser.query(
+          'form.edit-password input[name="password"]'
+        ).should.be.ok;
+
+        // Confirm input and value.
+        browser.query(
+          'form.edit-password input[name="confirm"]'
+        ).should.be.ok;
+
+        done();
+
+      });
+
+    });
+
+  });
+
+  describe('POST /admin/users/edit/:username/info', function() {
+
     beforeEach(function(done) {
 
       // Go to non-self user edit page.
@@ -523,60 +624,215 @@ describe('User Controller', function() {
 
     });
 
-    it('should render the edit information form', function(done) {
-
-      // Check for form.
-      browser.query('form.edit-info').should.be.ok;
-
-      // Username input and value.
-      browser.query('form.edit-info input[name="username"]').should.be.ok;
-
-      // Email input and value.
-      browser.query('form.edit-info input[name="email"]').should.be.ok;
-
-      // Super user input and value.
-      browser.query('form.edit-info input[name="superUser"]').should.be.ok;
-
-      // Active input and value.
-      browser.query('form.edit-info input[name="active"]').should.be.ok;
-
-      done();
-
-    });
-
-    it('should not render the Super User and Active checkboxes for self edit');
-
-    it('should render the edit password form');
-
-  });
-
-  describe('POST /admin/users/edit/:username/info', function() {
-
     describe('username', function() {
 
-      it('should flash error for no username');
+      it('should flash error for no username', function(done) {
 
-      it('should flash error for taken username');
+        // GET admin/users.
+        browser.visit(r+'admin/users/edit/kara', function() {
 
-      it('should flash error for username < 4 characters');
+          // Fill in form.
+          browser.fill('username', '');
+          browser.pressButton('form.edit-info button', function() {
 
-      it('should flash error for username > 20 characters');
+            // Check for error.
+            browser.location.pathname.should.eql('/admin/users/edit/kara/info');
+            browser.query('span.help-inline.username').should.be.ok;
+            done();
 
-      it('should not flash error for self username');
+          });
 
-      it('should not flash error for different username');
+        });
+
+      });
+
+      it('should flash error for taken username', function(done) {
+
+        // GET admin/users.
+        browser.visit(r+'admin/users/edit/kara', function() {
+
+          // Fill in form.
+          browser.fill('username', 'david');
+          browser.pressButton('form.edit-info button', function() {
+
+            // Check for error.
+            browser.location.pathname.should.eql('/admin/users/edit/kara/info');
+            browser.query('span.help-inline.username').should.be.ok;
+            done();
+
+          });
+
+        });
+
+      });
+
+      it('should flash error for username < 4 characters', function(done) {
+
+        // GET admin/users.
+        browser.visit(r+'admin/users/edit/kara', function() {
+
+          // Fill in form.
+          browser.fill('username', 'dav');
+          browser.pressButton('form.edit-info button', function() {
+
+            // Check for error.
+            browser.location.pathname.should.eql('/admin/users/edit/kara/info');
+            browser.query('span.help-inline.username').should.be.ok;
+            done();
+
+          });
+
+        });
+
+      });
+
+      it('should flash error for username > 20 characters', function(done) {
+
+        // GET admin/users.
+        browser.visit(r+'admin/users/edit/kara', function() {
+
+          // Fill in form.
+          browser.fill('username', 'srdavidwilliamcclurejr');
+          browser.pressButton('form.edit-info button', function() {
+
+            // Check for error.
+            browser.location.pathname.should.eql('/admin/users/edit/kara/info');
+            browser.query('span.help-inline.username').should.be.ok;
+            done();
+
+          });
+
+        });
+
+      });
+
+      it('should not flash error for self username', function(done) {
+
+        // GET admin/users.
+        browser.visit(r+'admin/users/edit/kara', function() {
+
+          // Fill in form (empty email to trigger form re-show).
+          browser.fill('username', 'kara');
+          browser.fill('email', '');
+          browser.pressButton('form.edit-info button', function() {
+
+            // Check for no error.
+            browser.location.pathname.should.eql('/admin/users/edit/kara/info');
+            assert(!browser.query('span.help-inline.username'));
+            done();
+
+          });
+
+        });
+
+      });
+
+      it('should not flash error for different username', function(done) {
+
+        // GET admin/users.
+        browser.visit(r+'admin/users/edit/kara', function() {
+
+          // Fill in form (empty email to trigger form re-show).
+          browser.fill('username', 'different');
+          browser.fill('email', '');
+          browser.pressButton('form.edit-info button', function() {
+
+            // Check for no error.
+            browser.location.pathname.should.eql('/admin/users/edit/kara/info');
+            assert(!browser.query('span.help-inline.username'));
+            done();
+
+          });
+
+        });
+
+      });
 
     });
 
     describe('email', function() {
 
-      it('should flash error for no email');
+      it('should flash error for no email', function(done) {
 
-      it('should flash error for invalid email');
+        // GET admin/users.
+        browser.visit(r+'admin/users/edit/kara', function() {
 
-      it('should not flash error for self email');
+          // Fill in form.
+          browser.fill('email', '');
+          browser.pressButton('form.edit-info button', function() {
 
-      it('should not flash error for different email');
+            // Check for error.
+            browser.location.pathname.should.eql('/admin/users/edit/kara/info');
+            browser.query('span.help-inline.email').should.be.ok;
+            done();
+
+          });
+
+        });
+
+      });
+
+      it('should flash error for invalid email', function(done) {
+
+        // GET admin/users.
+        browser.visit(r+'admin/users/edit/kara', function() {
+
+          // Fill in form.
+          browser.fill('email', 'invalid');
+          browser.pressButton('form.edit-info button', function() {
+
+            // Check for error.
+            browser.location.pathname.should.eql('/admin/users/edit/kara/info');
+            browser.query('span.help-inline.email').should.be.ok;
+            done();
+
+          });
+
+        });
+
+      });
+
+      it('should not flash error for self email', function(done) {
+
+        // GET admin/users.
+        browser.visit(r+'admin/users/edit/kara', function() {
+
+          // Fill in form (empty username to trigger form re-show).
+          browser.fill('email', 'kara@test.com');
+          browser.fill('username', '');
+          browser.pressButton('form.edit-info button', function() {
+
+            // Check for error.
+            browser.location.pathname.should.eql('/admin/users/edit/kara/info');
+            assert(!browser.query('span.help-inline.email'));
+            done();
+
+          });
+
+        });
+
+      });
+
+      it('should not flash error for different email', function(done) {
+
+        // GET admin/users.
+        browser.visit(r+'admin/users/edit/kara', function() {
+
+          // Fill in form (empty username to trigger form re-show).
+          browser.fill('email', 'new@test.com');
+          browser.fill('username', '');
+          browser.pressButton('form.edit-info button', function() {
+
+            // Check for error.
+            browser.location.pathname.should.eql('/admin/users/edit/kara/info');
+            assert(!browser.query('span.help-inline.email'));
+            done();
+
+          });
+
+        });
+
+      });
 
     });
 

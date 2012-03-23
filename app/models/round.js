bf33460c49b5fe7278f376f763bd997cb7114dbe
David McClure <david.mcclure@virginia.edu>
2012-03-22 21:51:24 -0400

    adds syllables hash

---
+++ app/models/round.js
@@ -0,0 +1,35 @@
+/*
+ * Round model.
+ */
+
+// Module dependencies.
+var slicer = require('../../lib/slicer'),
+  _ = require('underscore');
+
+// Schema definition.
+var Round = new Schema({
+  poem :            { type: Schema.ObjectId, ref: 'Poem', required: true },
+  created :         { type: Date, default: Date.now(), required: true }
+});
+
+
+/*
+ * -----------------
+ * Document methods.
+ * -----------------
+ */
+
+
+/*
+ * Get id.
+ *
+ * @return {String}: The id.
+ */
+Round.virtual('id').get(function() {
+  return this._id.toHexString();
+});
+
+
+// Register model.
+mongoose.model('Round', Round);
+var Round = mongoose.model('Round');

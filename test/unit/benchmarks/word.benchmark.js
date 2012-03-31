1840baf2d7c55c685697b84e5511e51cbf9948f0
David McClure <david.mcclure@virginia.edu>
2012-03-31 15:27:54 -0400

    adds benchmark for word.score()

---
+++ test/unit/benchmarks/word.benchmark.js
@@ -0,0 +1,57 @@
+/*
+ * Performance benchmark for the vote score() method.
+ *
+ * To run: >> node word.benchmark.js {#votes}
+ */
+
+// Module dependencies.
+var async = require('async');
+
+// Boostrap the database.
+process.env.NODE_ENV = 'testing';
+require('../db-connect');
+
+// Models.
+require('../../../app/models/word');
+require('../../../app/models/vote');
+var Word = mongoose.model('Word');
+var Vote = mongoose.model('Vote');
+
+// Create word.
+var word = new Word({
+  word: 'test'
+});
+
+// Get iterations.
+var i = process.argv[2];
+
+// Save queue for votes.
+var votes = [];
+
+// Create votes.
+while (i--) {
+  votes.push(new Vote({
+    word: word.id,
+    quantity: 100
+  }));
+}
+
+// Save worker.
+var save = function(document, callback) {
+  document.save(function(err) {
+    callback(null, document);
+  });
+};
+
+// Save votes.
+async.map(votes, save, function(err, documents) {
+
+  var t1 = Date.now();
+  word.score(Date.now() + 60000, 60000, function(score) {
+    var t2 = Date.now();
+    console.log(score);
+    console.log('Total Duration: %d', t2-t1);
+    process.exit();
+  });
+
+});

25780367197f23470a5225dad56b315c98a4afa6
David McClure <david.mcclure@virginia.edu>
2012-05-09 09:17:55 -0400

    emitters for word highlight and select

--- public/javascripts/oversoul/views/word-view.js
+++ public/javascripts/oversoul/views/word-view.js
@@ -11,27 +11,59 @@ Ov.Views.Word = Backbone.View.extend({
     return _.template($('#stack-word').html());
   },
 
+  events: {
+    'mouseenter .stack-word':   'onHighlight',
+    'mousedown .stack-word':    'onSelect'
+  },
+
   /*
    * Build template, get components.
    *
    * @return void.
    */
   initialize: function() {
+
+    // Inject template.
     this.$el.append(this.template()());
-    this.value = this.$el.find('.stack-value');
-    this.word = this.$el.find('.stack-word');
+
+    // Get components
+    this.valueMarkup = this.$el.find('.stack-value');
+    this.wordMarkup = this.$el.find('.stack-word');
+
+    // Trackers.
+    this.word = null;
+
   },
 
   /*
-   * Render new values.
+   * Render new values, set word tracker.
    *
    * @param {Array} data: Array of [word, value].
    *
    * @return void.
    */
   update: function(data) {
-    this.word.text(data[0]);
-    this.value.text(data[1]);
+    this.wordMarkup.text(data[0]);
+    this.valueMarkup.text(data[1]);
+    this.word = data[0];
+  },
+
+  /*
+   * On hover on the word text.
+   *
+   * @return void.
+   */
+  onHighlight: function() {
+    Ov.vent.trigger('stacks:highlight', this.word);
+  },
+
+  /*
+   * On click on the word text.
+   *
+   * @return void.
+   */
+  onSelect: function() {
+    Ov.vent.trigger('stacks:select', this.word);
   }
 
 });

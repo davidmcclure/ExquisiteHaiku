5d2101f16968f171d4b03a4536b8c59c2260695d
David McClure <david.mcclure@virginia.edu>
2012-06-18 00:10:15 -0400

    proof of concept benchmark for new scoring approach

--- test/unit/benchmarks/vote.benchmark.js
+++ test/unit/benchmarks/vote.benchmark.js
@@ -27,10 +27,39 @@ var d = 60000;
 var id = 1/d;
 
 var t1 = Date.now();
-while (i--) { vote.score(t, d, id); }
+// while (i--) { vote.score(t, d, id); }
+// while (i--) { score(t, d, id, vote.applied, vote.quantity); }
+var test = [1339991346551, 300];
+for(var j=0; j<i; j++) {
+  score(t, d, id, test[0], test[1]);
+}
 var t2 = Date.now();
 
 console.log('%d votes', process.argv[2]);
 console.log('Duration: %d', t2-t1);
 
 process.exit();
+
+
+
+function score(now, decayL, decayI, applied, quantity) {
+
+  // Get time delta.
+  var delta = now - applied;
+
+  // Compute unscaled decay coefficient.
+  var decay = Math.exp(-delta * decayI);
+
+  // Compute churn.
+  var churn = Math.round(quantity * decay);
+
+  // Starting boundaries.
+  var bound1 = quantity * -decayL;
+  var bound2 = bound1 * decay;
+
+  // Get the integral, scale and round.
+  var rank = Math.round(((bound2-bound1)*0.001));
+
+  return { rank: rank, churn: churn };
+
+}

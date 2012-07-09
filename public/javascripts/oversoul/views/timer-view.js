e9aa8a0f0dda74f9088e0ffce703f1654a6a1995
David McClure <david.mcclure@virginia.edu>
2012-07-09 17:34:02 -0400

    timer, but poor performance

--- public/javascripts/oversoul/views/timer-view.js
+++ public/javascripts/oversoul/views/timer-view.js
@@ -6,12 +6,28 @@ Ov.Views.Timer = Backbone.View.extend({
 
   el: '#timer',
 
+  options: {
+    refresh: 100 // Ms between renders.
+  },
+
+  template: function() {
+    return _.template($('#clock').html());
+  },
+
   /*
-   * .
+   * Set rendering heartbeat.
    *
    * @return void.
    */
   initialize: function() {
+
+    // Initialize update trackers.
+    this.updateTime = null;
+    this.updateQuantity = null;
+
+    // Set the interval.
+    var callback = _.bind(this.render, this);
+    setInterval(callback, this.options.refresh);
 
   },
 
@@ -23,7 +39,32 @@ Ov.Views.Timer = Backbone.View.extend({
    * @return void.
    */
   update: function(duration) {
-    this.$el.text(duration);
+    this.updateTime = Date.now();
+    this.updateQuantity = duration;
+  },
+
+  /*
+   * Render the human-readable time remaining.
+   *
+   * @return void.
+   */
+  render: function() {
+
+    // Get time parts.
+    var current = this._msToDuration(
+      this.updateQuantity - (Date.now() - this.updateTime)
+    );
+
+    // Render.
+    this.$el.html(
+      this.template()({
+        hrs: current[0],
+        min: current[1],
+        sec: current[2],
+        mls: current[3]
+      })
+    );
+
   },
 
   /*
@@ -32,7 +73,7 @@ Ov.Views.Timer = Backbone.View.extend({
    *
    * @param {Number} d: The duration in milliseconds.
    *
-   * @return void.
+   * @return {Array}: [hs, min, sec, mls].
    */
   _msToDuration: function(d) {
     var mls = Math.floor(d % 1000); d /= 1000;

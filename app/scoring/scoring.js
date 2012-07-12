17e87ed24da27d03fc0d4cf7aec851f6d34768c3
David McClure <david.mcclure@virginia.edu>
2012-07-12 10:54:00 -0400

    poem

--- app/scoring/scoring.js
+++ app/scoring/scoring.js
@@ -128,6 +128,42 @@ var compute = exports.compute = function(
 
   return [rank, churn];
 
+  /*
+  Quixote and the Stone
+
+  In the right light you can see that from my ears
+  Issue into the world two great arcs of joy and mourn
+  Curving widely and slowly into the sky
+  Straining towards identity across the loop.
+
+  My steed bore me to the grasses of a small meadow,
+  High in the plains, where I discovered a stone.
+  I divined it to be a crumb lost from the howling tables
+  Of all the scuttling and wonderful monsters
+  Who keep me company through the night,
+  And resolved, as was my wont, to unmake its plight.
+
+  I thrust my sword into the soil
+  And set the pebble atop the skyward crown of the hilt
+  And conjured swirling and singing winds
+  In the world behind my eyes
+  And bestowed on my little ward
+  A license and duty to make haste to heaven.
+
+  I tore out my beard and found myself covered in blood
+  And crosslegged in the grass
+  And leaned backwards into a long nap.
+  When I woke it was night,
+  And Rocinante was nibbling on mushrooms,
+  And was glad to see me.
+
+  I recovered the stone, which I had cast back down,
+  And passed the night under the bright Spanish stars
+  Grinding it to a dust under the spur of my boot.
+  At dawn I pinched the little mound in my fingers
+  And threw it into the wind.
+  */
+
 };
 
 

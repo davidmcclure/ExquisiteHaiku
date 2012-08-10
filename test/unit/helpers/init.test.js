279126b5db0edc8335b0691b8ca56c4a1877daf9
David McClure <david.mcclure@virginia.edu>
2012-08-10 15:12:20 -0400

    shell for init unit tests

---
+++ test/unit/helpers/init.test.js
@@ -0,0 +1,34 @@
+/*
+ * Unit tests for application startup.
+ */
+
+// Module dependencies.
+var mocha = require('mocha');
+var should = require('should');
+var async = require('async');
+var assert = require('assert');
+var sinon = require('sinon');
+
+// Boostrap the application.
+process.env.NODE_ENV = 'testing';
+require('../../db-connect');
+
+
+/*
+ * ----------------
+ * Init unit tests.
+ * ----------------
+ */
+
+
+describe('Init', function() {
+
+  describe('run', function() {
+
+  });
+
+  describe('startPoems', function() {
+
+  });
+
+});

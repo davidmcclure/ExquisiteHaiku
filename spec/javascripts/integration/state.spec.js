0535a124937449fcfb5e69cac36bfea0f3147b9b
David McClure <david.mcclure@virginia.edu>
2012-08-21 17:02:06 -0400

    tests for submit -> vote

--- spec/javascripts/integration/state.spec.js
+++ spec/javascripts/integration/state.spec.js
@@ -4,7 +4,7 @@
 
 describe('State', function() {
 
-  var blank, stack;
+  var poem, blank, stack, log, slice;
 
   // Get fixtures.
   beforeEach(function() {
@@ -13,8 +13,10 @@ describe('State', function() {
 
   // Shortcut classes.
   beforeEach(function() {
+    poem = Ov.Controllers.Poem.Poem;
     blank = Ov.Controllers.Poem.Blank;
     stack = Ov.Controllers.Stack.Rank;
+    log = Ov.Controllers.Log.Stack;
   });
 
   // Clear localstorage.
@@ -24,15 +26,111 @@ describe('State', function() {
 
   describe('submit -> vote', function() {
 
+    var e;
+
+    beforeEach(function() {
+
+      // Mock keypress.
+      e = $.Event('keyup');
+
+      // Activate voting. 
+      blank.activateVote();
+
+      // Set minSubmissions.
+      Poem.minSubmissions = 3;
+
+      slice = {
+        stack: [],
+        syllables: 0,
+        round: 'id',
+        poem: [],
+        clock: 10000
+      };
+
+      // // Incoming data slice.
+      Ov.vent.trigger('socket:slice', slice);
+
+      // Add words to stack.
+      e.keyCode = 13;
+      blank.$el.val('valid1');
+      blank.$el.trigger(e);
+      blank.$el.val('valid2');
+      blank.$el.trigger(e);
+      blank.$el.val('valid3');
+      blank.$el.trigger(e);
+
+    });
+
     it('should clear the submission stack');
 
-    it('should disable the blank');
+    it('should disable the blank', function() {
 
-    it('should clear the blank');
+      // Trigger submit.
+      e.keyCode = 17;
+      blank.$el.trigger(e);
 
-    it('should start rendering the stack');
+      // Trigger slice.
+      Ov.vent.trigger('socket:slice', slice);
 
-    it('should start rendering the log');
+      // Check for empty stack
+      expect(blank.$el).toBeDisabled();
+
+    });
+
+    it('should clear the blank', function() {
+
+      // Trigger submit.
+      e.keyCode = 17;
+      blank.$el.val('word');
+      blank.$el.trigger(e);
+
+      // Trigger slice.
+      Ov.vent.trigger('socket:slice', slice);
+
+      // Check for empty stack
+      expect(blank.$el.val()).toEqual('');
+
+    });
+
+    it('should start rendering the stack', function() {
+
+      // Trigger submit.
+      e.keyCode = 17;
+      blank.$el.trigger(e);
+
+      // Mock stack.
+      slice.stack = [
+        ['word1', 100, 50, -51, '1.00'],
+        ['word2', 99, 40, -41, '0.99']
+      ];
+
+      // Trigger slice.
+      Ov.vent.trigger('socket:slice', slice);
+
+      // Get word rows.
+      var rows = stack.$el.find('div.stack-row');
+      expect(rows.length).toEqual(2);
+
+    });
+
+    it('should start rendering the log', function() {
+
+      // Trigger submit.
+      e.keyCode = 17;
+      blank.$el.trigger(e);
+
+      // Trigger slice.
+      Ov.vent.trigger('socket:slice', slice);
+
+      // Ingest votes.
+      Ov.vent.trigger('socket:vote:in', 'word1', 100);
+      Ov.vent.trigger('socket:vote:in', 'word2', 200);
+
+      // Get word rows.
+      var rows = log.primaryMarkup.find('div.log-row');
+      expect(rows.length).toEqual(2);
+
+    });
 
   });
 

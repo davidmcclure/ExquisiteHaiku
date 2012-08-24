3d798d110efcd8f2951c004e7c9d530f087cb5ae
David McClure <david.mcclure@virginia.edu>
2012-08-24 10:00:11 -0400

    first pass on new runner

--- spec/javascripts/support/phantom-runner.js
+++ spec/javascripts/support/phantom-runner.js
@@ -1,79 +1,112 @@
 var system = require('system');
 
 /**
- * Wait until the test condition is true or a timeout occurs. Useful for waiting
- * on a server response or for a ui change (fadeIn, etc.) to occur.
+ * Poll for a condition on the page.
  *
- * @param testFx javascript condition that evaluates to a boolean,
- * it can be passed in as a string (e.g.: "1 == 1" or "$('#bar').is(':visible')" or
- * as a callback function.
- * @param onReady what to do when testFx condition is fulfilled,
- * it can be passed in as a string (e.g.: "1 == 1" or "$('#bar').is(':visible')" or
- * as a callback function.
- * @param timeOutMillis the max amount of time to wait. If not specified, 3 sec is used.
+ * @param {Function} testFx: Condition that evaluates to a boolean
+ * @param {Function} onReady: Runs when condition is fulfilled.
+ * @param {Number} timeOut: Max timeout in miliseconds.
  */
-function waitFor(testFx, onReady, timeOutMillis) {
-    var maxtimeOutMillis = timeOutMillis ? timeOutMillis : 3001, //< Default Max Timeout is 3s
-        start = new Date().getTime(),
-        condition = false,
-        interval = setInterval(function() {
-            if ( (new Date().getTime() - start < maxtimeOutMillis) && !condition ) {
-                // If not time-out yet and condition not yet fulfilled
-                condition = (typeof(testFx) === "string" ? eval(testFx) : testFx()); //< defensive code
-            } else {
-                if(!condition) {
-                    // If condition still not fulfilled (timeout but condition is 'false')
-                    console.log("'waitFor()' timeout");
-                    phantom.exit(1);
-                } else {
-                    // Condition fulfilled (timeout and/or condition is 'true')
-                    console.log("'waitFor()' finished in " + (new Date().getTime() - start) + "ms.");
-                    typeof(onReady) === "string" ? eval(onReady) : onReady(); //< Do what it's supposed to do once the condition is fulfilled
-                    clearInterval(interval); //< Stop this interval
-                }
-            }
-        }, 100); //< repeat check every 100ms
-};
+function waitFor(testFx, onReady, timeOut) {
 
+  // Starting parameters.
+  var maxWait = timeOut ? timeOut : 3000;
+  var start = Date.now();
+  var condition = false;
+
+  // Set the interval.
+  var interval = setInterval(function() {
+
+    // If maxWait has not elapsed.
+    if (Date.now() - start < maxWait && !condition) {
+      condition = testFx();
+    }
+
+    else {
+
+      // If condition is still false.
+      if (!condition) {
+        console.log('waitFor() timeout');
+        phantom.exit();
+      }
+
+      else {
+        onReady();
+        clearInterval(interval);
+      }
+
+    }
+
+  }, 100);
+
+}
 
 if (system.args.length !== 2) {
-    console.log('Usage: run-jasmine.js URL');
-    phantom.exit(1);
+  console.log('Usage: phantom-runner.js {URL}');
+  phantom.exit(1);
 }
 
 var page = require('webpage').create();
 
-// Route "console.log()" calls from within the Page context to the main Phantom context (i.e. current "this")
+// Pipe page console logs to terminal.
 page.onConsoleMessage = function(msg) {
-    console.log(msg);
+  console.log(msg);
 };
 
-page.open(system.args[1], function(status){
-    if (status !== "success") {
-        console.log("Unable to access network");
-        phantom.exit();
-    } else {
-        waitFor(function(){
-            return page.evaluate(function(){
-                if (document.body.querySelector('.runner .description')) {
-                    return true;
-                }
-                return false;
-            });
-        }, function(){
-            page.evaluate(function(){
-                console.log(document.body.querySelector('.description').innerText);
-                list = document.body.querySelectorAll('div.jasmine_reporter > div.suite.failed');
-                for (i = 0; i < list.length; ++i) {
-                    el = list[i];
-                    desc = el.querySelectorAll('.description');
-                    console.log('');
-                    for (j = 0; j < desc.length; ++j) {
-                        console.log(desc[j].innerText);
-                    }
-                }
-            });
-            phantom.exit();
-        });
-    }
+// Run the suite.
+page.open(system.args[1], function(status) {
+
+  if (status !== 'success') {
+    console.log('Unable to open tests.');
+    phantom.exit();
+  }
+
+  else {
+
+    waitFor(function() {
+      return page.evaluate(function() {
+        if ($('.passingAlert'))
+          return true;
+        return false;
+      });
+    }, function() {
+      page.evaluate(function() {
+        console.log($('.passingAlert').text());
+      });
+      phantom.exit();
+    }, 3000);
+
+  }
+
+    // if (status !== "success") {
+    //     console.log("Unable to access network");
+    //     phantom.exit();
+    // } else {
+    //     waitFor(function(){
+    //         return page.evaluate(function(){
+    //             if (document.body.querySelector('.passingAlert')) {
+    //                 return true;
+    //             }
+    //             return false;
+    //         });
+    //     }, function(){
+    //       page.evaluate(function() {
+    //         console.log(document.body.querySelector('.passingAlert').innerText);
+    //       });
+    //         page.evaluate(function(){
+    //             console.log(document.body.querySelector('.description').innerText);
+    //             list = document.body.querySelectorAll('div.jasmine_reporter > div.suite.failed');
+    //             for (i = 0; i < list.length; ++i) {
+    //                 el = list[i];
+    //                 desc = el.querySelectorAll('.description');
+    //                 console.log('');
+    //                 for (j = 0; j < desc.length; ++j) {
+    //                     console.log(desc[j].innerText);
+    //                 }
+    //             }
+    //         });
+    //         phantom.exit();
+    //     });
+    // }
+
 });

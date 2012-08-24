80cde1b5d97bb32192a51d7f36d68883977889d2
David McClure <david.mcclure@virginia.edu>
2012-08-24 12:21:19 -0400

    progress on jasmine reporter

--- spec/javascripts/support/phantom-runner.js
+++ spec/javascripts/support/phantom-runner.js
@@ -63,55 +63,39 @@ page.open(system.args[1], function(status) {
 
   else {
 
+    console.log('Running Jasmine suite:');
     waitFor(function() {
       return page.evaluate(function() {
-        if ($('.passingAlert'))
+        if ($('.passingAlert') || $('.failingAlert'))
           return true;
         return false;
       });
     }, function() {
       page.evaluate(function() {
+
+        // Build dots.
         var symbols = '';
         $('ul.symbolSummary li').each(function(i, li) {
           if ($(li).hasClass('passed')) symbols+='.';
           else if ($(li).hasClass('failed')) symbols+='x';
         });
         console.log(symbols);
+
+        // If passing.
+        var passingAlert = $('.passingAlert');
+        if (passingAlert.length) {
+          console.log(passingAlert.text());
+        }
+
+        // Failing.
+        else {
+          console.log($('.failingAlert').text());
+        }
+
       });
       phantom.exit();
     }, 3000);
 
   }
-
-    // if (status !== "success") {
-    //     console.log("Unable to access network");
-    //     phantom.exit();
-    // } else {
-    //     waitFor(function(){
-    //         return page.evaluate(function(){
-    //             if (document.body.querySelector('.passingAlert')) {
-    //                 return true;
-    //             }
-    //             return false;
-    //         });
-    //     }, function(){
-    //       page.evaluate(function() {
-    //         console.log(document.body.querySelector('.passingAlert').innerText);
-    //       });
-    //         page.evaluate(function(){
-    //             console.log(document.body.querySelector('.description').innerText);
-    //             list = document.body.querySelectorAll('div.jasmine_reporter > div.suite.failed');
-    //             for (i = 0; i < list.length; ++i) {
-    //                 el = list[i];
-    //                 desc = el.querySelectorAll('.description');
-    //                 console.log('');
-    //                 for (j = 0; j < desc.length; ++j) {
-    //                     console.log(desc[j].innerText);
-    //                 }
-    //             }
-    //         });
-    //         phantom.exit();
-    //     });
-    // }
 
 });

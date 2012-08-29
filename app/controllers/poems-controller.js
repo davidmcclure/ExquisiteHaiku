e4a0a5daaba102dac2838bfb50fcf6e3bcc66f8b
David McClure <david.mcclure@virginia.edu>
2012-08-28 21:31:10 -0400

    before poem update refactor

--- app/controllers/poems-controller.js
+++ app/controllers/poems-controller.js
@@ -22,15 +22,13 @@ module.exports = function(app) {
    *
    * @middleware auth.getPoem: Pass the poem identified by :slug.
    */
-  app.get('/:hash',
-    auth.getPoem,
-    function(req, res) {
+  app.get('/:hash', auth.getPoem, function(req, res) {
 
-      // Render the layout.
-      res.render('poem/index', {
-        title:  req.params.hash,
-        poem:   JSON.stringify(req.poem)
-      });
+    // Render the layout.
+    res.render('poem/index', {
+      title:  req.params.hash,
+      poem:   JSON.stringify(req.poem)
+    });
 
   });
 

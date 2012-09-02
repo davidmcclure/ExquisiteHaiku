1668645cdd69a99363f5ccbec095558130c1681e
David McClure <david.mcclure@virginia.edu>
2012-09-02 18:48:08 -0400

    gets rid of old admin controller routes and tests

--- app/controllers/admin-controller.js
+++ app/controllers/admin-controller.js
@@ -50,9 +50,9 @@ module.exports = function(app, io) {
 
         // Render the list.
         res.render('admin/index', {
-          title:  'Oversoul',
-          user:   req.user,
-          poems:  poems
+          title: 'Oversoul',
+          user: req.user,
+          poems: poems
         });
 
       });
@@ -70,9 +70,9 @@ module.exports = function(app, io) {
 
       // Render the form.
       res.render('admin/new', {
-        title:  'New Poem',
-        user:   req.user,
-        form:   poemForm.form()
+        title: 'New Poem',
+        user: req.user,
+        form: poemForm.form()
       });
 
   });
@@ -116,197 +116,13 @@ module.exports = function(app, io) {
 
           // Re-render the form.
           res.render('admin/new', {
-            title:  'New Poem',
-            user:   req.user,
-            form:   form
+            title: 'New Poem',
+            user: req.user,
+            form: form
           });
 
         }
 
-      });
-
-  });
-
-  /*
-   * Edit a poem.
-   *
-   * @middleware auth.isUser: Block if there is no user session.
-   * @middleware auth.getPoem: Pass the poem identified by :slug.
-   * @middleware auth.ownsPoem: Pass if the poem belongs to the user.
-   * @middleware auth.unstartedPoem: Block if the poem is started.
-   */
-  app.get('/admin/poems/edit/:hash',
-    auth.isUser,
-    auth.getPoem,
-    auth.ownsPoem,
-    auth.unstartedPoem,
-    function(req, res) {
-
-      var form = poemForm.form(req.poem).bind({
-        slug:           req.poem.slug,
-        roundLength:    req.poem.roundLength,
-        sliceInterval:  req.poem.sliceInterval,
-        minSubmissions: req.poem.minSubmissions,
-        submissionVal:  req.poem.submissionVal,
-        decayLifetime:  req.poem.decayLifetime,
-        seedCapital:    req.poem.seedCapital,
-        visibleWords:   req.poem.visibleWords
-      });
-
-      // Render the form.
-      res.render('admin/edit', {
-        title:  'Edit Poem',
-        user:   req.user,
-        poem:   req.poem,
-        form:   form
-      });
-
-  });
-
-  /*
-   * Handle edit form.
-   *
-   * @middleware auth.isUser: Block if there is no user session.
-   * @middleware auth.getPoem: Pass the poem identified by :slug.
-   * @middleware auth.ownsPoem: Pass if the poem belongs to the user.
-   * @middleware auth.unstartedPoem: Block if the poem is started.
-   */
-  app.post('/admin/poems/edit/:hash',
-    auth.isUser,
-    auth.getPoem,
-    auth.ownsPoem,
-    auth.unstartedPoem,
-    function(req, res) {
-
-      // Pass control to the form.
-      poemForm.form(req.poem).handle(req, {
-
-        // If field validations pass.
-        success: function(form) {
-
-          // Update the poem.
-          req.poem.slug =             form.data.slug;
-          req.poem.roundLength =      form.data.roundLength;
-          req.poem.minSubmissions =   form.data.minSubmissions;
-          req.poem.submissionVal =    form.data.submissionVal;
-          req.poem.decayLifetime =    form.data.decayLifetime;
-          req.poem.seedCapital =      form.data.seedCapital;
-
-          // Save and redirect.
-          req.poem.save(function(err) {
-            res.redirect('/admin/poems');
-          });
-
-        },
-
-        // If field validations fail.
-        other: function(form) {
-
-          // Re-render the form.
-          res.render('admin/edit', {
-            title:  'Edit Poem',
-            user:   req.user,
-            poem:   req.poem,
-            form:   form
-          });
-
-        }
-
-      });
-
-  });
-
-  /*
-   * Delete poem confirmation page.
-   *
-   * @middleware auth.isUser: Block if there is no user session.
-   * @middleware auth.getPoem: Pass the poem identified by :slug.
-   * @middleware auth.ownsPoem: Pass if the poem belongs to the user.
-   */
-  app.get('/admin/poems/delete/:hash',
-    auth.isUser,
-    auth.getPoem,
-    auth.ownsPoem,
-    function(req, res) {
-
-      // Render the form.
-      res.render('admin/delete', {
-        title:  'Delete Poem',
-        user:   req.user,
-        poem:   req.poem
-      });
-
-  });
-
-  /*
-   * Delete poem.
-   *
-   * @middleware auth.isUser: Block if there is no user session.
-   * @middleware auth.getPoem: Pass the poem identified by :slug.
-   * @middleware auth.ownsPoem: Pass if the poem belongs to the user.
-   */
-  app.post('/admin/poems/delete/:hash',
-    auth.isUser,
-    auth.getPoem,
-    auth.ownsPoem,
-    function(req, res) {
-
-      // Stop poem..
-      req.poem.stop();
-
-      // Remove and redirect.
-      req.poem.remove(function(err) {
-        res.redirect('/admin/poems');
-      });
-
-  });
-
-  /*
-   * Start poem.
-   *
-   * @middleware auth.isUser: Block if there is no user session.
-   * @middleware auth.getPoem: Pass the poem identified by :slug.
-   * @middleware auth.ownsPoem: Pass if the poem belongs to the user.
-   */
-  app.get('/admin/poems/start/:hash',
-    auth.isUser,
-    auth.getPoem,
-    auth.ownsPoem,
-    function(req, res) {
-
-      // Get broadcast callback.
-      var emit = scoring.getEmitter(io, req.poem.id);
-
-      // Create starting round, start.
-      if (req.poem.unstarted) req.poem.newRound();
-      req.poem.start(scoring.execute, emit, function() {});
-
-      // Save and redirect.
-      req.poem.save(function(err) {
-        res.redirect('/admin/poems');
-      });
-
-  });
-
-  /*
-   * Stop poem.
-   *
-   * @middleware auth.isUser: Block if there is no user session.
-   * @middleware auth.getPoem: Pass the poem identified by :slug.
-   * @middleware auth.ownsPoem: Pass if the poem belongs to the user.
-   */
-  app.get('/admin/poems/stop/:hash',
-    auth.isUser,
-    auth.getPoem,
-    auth.ownsPoem,
-    function(req, res) {
-
-      // Stop poem.
-      req.poem.stop();
-
-      // Save and redirect.
-      req.poem.save(function(err) {
-        res.redirect('/admin/poems');
       });
 
   });

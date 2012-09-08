14d0226398e1bff48b685d79bc47d78f59bf9e66
David McClure <david.mcclure@virginia.edu>
2012-09-08 13:50:52 -0400

    component getters for add form view

--- public/javascripts/apps/create/views/form-view.js
+++ public/javascripts/apps/create/views/form-view.js
@@ -4,13 +4,26 @@
 
 Create.Views.Form = Backbone.View.extend({
 
+  el: '#new',
+
   /*
    * Get components.
    *
    * @return void.
    */
   initialize: function() {
-    console.log('form');
+
+    // Get inputs.
+    this.rLenVal = this.$el.find('input.rLenVal');
+    this.rLenUnit = this.$el.find('input.rLenUnit');
+    this.rLenSec = this.$el.find('button.seconds');
+    this.rLenMin = this.$el.find('button.minutes');
+    this.seedCap = this.$el.find('input.seedCap');
+    this.minSub = this.$el.find('input.minSub');
+    this.subVal = this.$el.find('input.subVal');
+    this.decLife = this.$el.find('input.decLife');
+    this.published = this.$el.find('input.published');
+
   }
 
 });

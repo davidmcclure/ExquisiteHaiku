44ecac9a9e3c3f12ad8652f37df8f012ac627342
David McClure <david.mcclure@virginia.edu>
2012-09-17 20:57:05 -0400

    dependency includer in fixtures generator

--- test/fixtures/fixtures.test.js
+++ test/fixtures/fixtures.test.js
@@ -2,43 +2,21 @@
  * Fixture generator for front-end test suite.
  */
 
-// Modules
-// -------
-var mocha = require('mocha');
-var config = require('yaml-config');
-var fs = require('fs');
-var jade = require('jade');
-
-
-// Config
-// ------
-
-var root = config.readConfig('test/config.yaml').root;
-
-
-// Run
-// ---
-
-process.env.NODE_ENV = 'testing';
-var server = require('../../app');
-
-
-// Specs
-// -----
+var _t = require('../dependencies.js');
 
 describe('Fixtures', function() {
 
   it('render index.jade', function(done) {
 
-    fs.readFile(
+    _t.fs.readFile(
       'app/views/poem/base.jade',
       'utf8', function(err, data) {
 
         // Compile template function.
-        var template = jade.compile(data);
+        var template = _t.jade.compile(data);
 
         // Write file.
-        fs.writeFile(
+        _t.fs.writeFile(
           'spec/javascripts/fixtures/base.html',
           template(), function(err) {
             done();
@@ -50,15 +28,15 @@ describe('Fixtures', function() {
 
   it('render templates.jade', function(done) {
 
-    fs.readFile(
+    _t.fs.readFile(
       'app/views/poem/templates.jade',
       'utf8', function(err, data) {
 
         // Compile template function.
-        var template = jade.compile(data);
+        var template = _t.jade.compile(data);
 
         // Write file.
-        fs.writeFile(
+        _t.fs.writeFile(
           'spec/javascripts/fixtures/templates.html',
           template(), function(err) {
             done();

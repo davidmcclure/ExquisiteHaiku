b2d3c2bcb7660590455a6435ee1185ac9c42b2fd
David McClure <david.mcclure@virginia.edu>
2012-09-28 15:54:53 -0400

    Edits to dragging css.

--- public/javascripts/applications/poem/views/line-view.js
+++ public/javascripts/applications/poem/views/line-view.js
@@ -197,6 +197,7 @@ Ov.Views.Line = Backbone.View.extend({
     if (currentTotal >= 0) {
       this.total.attr('class', 'positive');
       this.circle.attr('class', 'positive');
+      $('body').css('background', 'positive');
     }
 
     // Negative.

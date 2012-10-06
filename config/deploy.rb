48305c5e4a2aeb0851960fee36326d7878197d13
David McClure <david.mcclure@virginia.edu>
2012-10-06 14:58:06 -0400

    Deployment setup working.

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,17 +10,16 @@ set :scm, :git
 set :branch, 'master'
 
 # Host information.
-set :host, "ec2-user@ec2-184-73-88-22.compute-1.amazonaws.com"
-set :deploy_via, :remote_cache
 default_run_options[:pty] = true
+set :host, "ec2-user@ec2-50-16-21-11.compute-1.amazonaws.com"
+set :deploy_via, :remote_cache
 set :use_sudo, true
+role :app, host
 
 # Node information.
 set :node_file, "app.js"
-set :user, "ec2-user"
+set :user, "root"
 
-role :app, host
-set :use_sudo, true
 set :bluepill, "path/to/bluepill"
 
 namespace :deploy do
@@ -43,10 +42,17 @@ namespace :deploy do
 
   desc "Create deployment directory"
   task :create_directory, :roles => :app, :except => { :no_release => true }  do
-    run "#{try_sudo} mkdir -p #{deploy_to}"
+    run "#{try_sudo :as => 'root'} mkdir -p #{deploy_to}"
+  end
+
+  desc "Set permissions on deployment directories"
+  task :set_permissions, :roles => :app, :except => { :no_release => true }  do
+    run "#{try_sudo :as => 'root'} chmod a+w -R #{deploy_to}"
   end
 
 end
 
+# Hooks.
 before 'deploy:setup', 'deploy:create_directory'
 before 'deploy:finalize_update', 'deploy:npm_install'
+after 'deploy:setup', 'deploy:set_permissions'
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,4 @@
 set :node_env, "production"
 set :deploy_to, "/var/www/apps/#{application}/#{node_env}"
 set :branch, "master"
-set :host, "ec2-user@ec2-184-73-88-22.compute-1.amazonaws.com"
 set :node_port, "3000"

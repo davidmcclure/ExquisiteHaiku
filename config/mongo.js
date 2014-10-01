bed21b524d5f2bfd8a007c4198fafd345afe4dcf
David McClure <davidwilliammcclure@gmail.com>
2014-10-01 11:12:48 -0700

    Logging the Mongo URI.

--- config/mongo.js
+++ config/mongo.js
@@ -5,7 +5,8 @@
 
 // Connect to Mongo.
 var mongoose = require('mongoose');
-mongoose.connect(process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGOLAB_URI || global.config.db);
+console.log(process.env.MONGOLAB_URI);
 
 // In-memory stores.
 global.Oversoul = { timers: {}, votes: {} };

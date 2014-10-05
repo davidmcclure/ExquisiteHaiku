425b34446cace4be1f64e67db3e8701b23ad936a
David McClure <davidwilliammcclure@gmail.com>
2014-10-04 20:50:23 -0700

    Lays the groundwork for a non-running poem viewer.

--- app/controllers/poem.js
+++ app/controllers/poem.js
@@ -16,11 +16,21 @@ module.exports = function(app) {
    */
   app.get('/poem/:hash', auth.getPoem, function(req, res) {
 
-    // Render the layout.
-    res.render('poem', {
-      title: req.params.hash,
-      poem: JSON.stringify(req.poem)
-    });
+    // If the poem is running, start the editor.
+    if (req.poem.running) {
+      res.render('poem', {
+        title: req.params.hash,
+        poem: JSON.stringify(req.poem)
+      });
+    }
+
+    // Otherwise, just print out the words.
+    else {
+      res.render('admin/poem', {
+        title: req.params.hash,
+        poem: req.poem
+      });
+    }
 
   });
 
